0bb8ffe130493e41723bffda9e5aea315225805f
Volker Simonis <volker.simonis@gmail.com>
2026-02-15 19:16:59 +0000

    Initial fixes

--- scripts/renaissanceNI.sh
+++ scripts/renaissanceNI.sh
@@ -35,7 +35,7 @@ echo_and_exec() {
 }
 
 if [[ ! -v OUTPUT ]]; then
-  OUTPUT="${MYPATH}/output"
+  OUTPUT="${MYPATH}/../output"
   echo "No OUTPUT specified, setting OUTPUT to '${OUTPUT}'"
 fi
 
@@ -130,10 +130,10 @@ jdk_ni_opts=(
   "" # GraalCE-NI
   "--native-compiler-options=-L${SHENANDOAH_HOME} --native-compiler-options=-Wl,--unresolved-symbols=ignore-all --gc=shenandoah -H:ShenandoahDebugLevel=product" # GraalCE-NI-SHEN
   "" # GraalEE-JIT
-  "-H-MLProfileInference" # GraalEE-NI
+  "-H:-MLProfileInference" # GraalEE-NI
   "" # GraalEE-NI-ML
   "" # GraalEE-NI-PGO
-  "--gc=G1 -H-MLProfileInference" # GraalEE-NI-G1
+  "--gc=G1 -H:-MLProfileInference" # GraalEE-NI-G1
   "--gc=G1" # GraalEE-NI-ML
   "--gc=G1" # GraalEE-NI-G1-PGO
 )
@@ -141,19 +141,19 @@ jdk_ni_opts=(
 # Special native-image runtime options for each 'mode' above (this array must have the sime size as 'modes' above)
 jdk_runtime_opts=(
   "" # OpenJDK
-  "" # GraalCE-JIT
+  "-XX:+EnableJVMCI -XX:+UseJVMCICompiler -XX:+UseJVMCINativeLibrary" # GraalCE-JIT
   "" # GraalCE-NI
   "-XX:+UnlockDiagnosticVMOptions -XX:ShenandoahGCMode=passive" # GraalCE-NI-SHEN
-  "" # GraalEE-JIT
+  "-XX:+EnableJVMCI -XX:+UseJVMCICompiler -XX:+UseJVMCINativeLibrary" # GraalEE-JIT
   "" # GraalEE-NI
   "" # GraalEE-NI-ML
   "" # GraalEE-NI-PGO
-  "--gc=G1" # GraalEE-NI-G1
-  "--gc=G1" # GraalEE-NI-G1-ML
-  "--gc=G1" # GraalEE-NI-G1-PGO
+  "" # GraalEE-NI-G1
+  "" # GraalEE-NI-G1-ML
+  "" # GraalEE-NI-G1-PGO
 )
 
-if [[ ${#modes[@]} != ${#jdks[@]} || ${#jdks[@]} != ${#jdk_ni_opts[@]} || ${#jdk_ni_opts[@]} != ${#jdk_runtime_opts[@]} ]]; do
+if [[ ${#modes[@]} != ${#jdks[@]} || ${#jdks[@]} != ${#jdk_ni_opts[@]} || ${#jdk_ni_opts[@]} != ${#jdk_runtime_opts[@]} ]]; then
   echo "'modes', 'jdks', 'jdk_ni_opts' and 'jdk_runtime_opts' should all have the same number of elements"
   exit 1
 fi
@@ -163,59 +163,62 @@ DATE=$(date "+%Y-%m-%d-%H-%M-%S")
 for b in ${!benchmarks[@]}; do
   benchmark=${benchmarks[$b]}
   benchmark_jar="${RENAISSANCE_SINGLE}/${benchmark}.jar"
+
   echo "=== ${benchmark} ==="
-  if [[ ! -e ${OUTPUT}/${benchmark}/conf ]]; then
-    echo_and_exec mkdir -p ${OUTPUT}/${benchmark}/conf
-  fi
-  if [[ ! -e ${OUTPUT}/${benchmark}/pgo ]]; then
-    echo_and_exec mkdir -p ${OUTPUT}/${benchmark}/pgo
-  fi
+
   if [[ ! -e ${OUTPUT}/results/${DATE}/${benchmark} ]]; then
     echo_and_exec mkdir -p ${OUTPUT}/results/${DATE}/${benchmark}
   fi
   # Create the NI metadata configuration first if it doesn't exist already
-  if [[ -v REBUILD_CONF || ! -e ${OUTPUT}/${benchmark}/conf/${benchmark}-conf ]]; then
-    echo_and_exec ${GRAALEE_HOME}/bin/java -agentlib:native-image-agent=config-output-dir=${OUTPUT}/${benchmark}/conf/${benchmark}-conf -jar ${benchmark_jar} --json /dev/null -t 60 ${benchmark}
+  if [[ -v REBUILD_CONF || ! -e ${OUTPUT}/${benchmark}/conf ]]; then
+    echo_and_exec ${GRAALEE_HOME}/bin/java -agentlib:native-image-agent=config-output-dir=${OUTPUT}/${benchmark}/conf -jar ${benchmark_jar} --json /dev/null -t 60 ${benchmark}
   fi
   for m in ${!modes[@]}; do
     jdk=${jdks[$m]}
+
+    if [[ ${benchmark} =~ "dotty" ]]; then
+      # 'dotty' needs special runtime arguments if run as native image
+      DOTTY_ARGS="-Djava.home=${jdk} -Djava.class.path=${benchmark_jar}"
+    else
+      DOTTY_ARGS=
+    fi
 
     if [[ ${modes[$m]} =~ "NI" ]]; then
 
       # For Native Image modes we have to do some preparations...
 
-      if [[ ${modes[$m]} =~ "PGO"]]; then
+      if [[ ${modes[$m]} =~ "PGO" ]]; then
 
         # For PGO modes, create an instrumented binary first.
         if [[ -v REBUILD_NI || ! -e ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]}.pgo ]]; then
-          echo_and_exec ${jdk}/bin/native-image -H:ConfigurationFileDirectories=${OUTPUT}/${benchmark}/conf/${benchmark}-conf -O3 ${ni_opts[$b]} ${jdk_ni_opts[$m]} --pgo-instrument  -o ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]}.pgo -cp ${benchmark_jar} org.renaissance.harness.RenaissanceSuite
+          echo_and_exec ${jdk}/bin/native-image -H:ConfigurationFileDirectories=${OUTPUT}/${benchmark}/conf -O3 ${ni_opts[$b]} ${jdk_ni_opts[$m]} --pgo-instrument  -o ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]}.pgo -cp ${benchmark_jar} org.renaissance.harness.RenaissanceSuite
         fi
 
         # Now run the instrumented binary to create the profile
         if [[ -v REBUILD_NI || ! -e ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]}.iprof ]]; then
-          echo_and_exec ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]}.pgo -XX:ProfilesDumpFile=${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]}.iprof ${JAVA_ARGS} -Dmode=${modes[$m]} ${jdk_runtime_opts[$m]} --json /dev/null -t 60 ${benchmark}
+          echo_and_exec ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]}.pgo -XX:ProfilesDumpFile=${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]}.iprof ${JAVA_ARGS} -Dmode=${modes[$m]} ${jdk_runtime_opts[$m]} ${DOTTY_ARGS} --json /dev/null -t 60 ${benchmark}
         fi
 
         # Create the native image based on the PGO profile if it doesn't exist already
         if [[ -v REBUILD_NI || ! -e ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]} ]]; then
-          echo_and_exec ${jdk}/bin/native-image --pgo=${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]}.iprof -H:ConfigurationFileDirectories=${OUTPUT}/${benchmark}/conf/${benchmark}-conf -O3 ${ni_opts[$b]} ${jdk_ni_opts[$m]} -o ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]} -cp ${benchmark_jar} org.renaissance.harness.RenaissanceSuite
+          echo_and_exec ${jdk}/bin/native-image --pgo=${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]}.iprof -H:ConfigurationFileDirectories=${OUTPUT}/${benchmark}/conf -O3 ${ni_opts[$b]} ${jdk_ni_opts[$m]} -o ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]} -cp ${benchmark_jar} org.renaissance.harness.RenaissanceSuite
         fi
 
       else # [[ ${modes[$m]} =~ "PGO"]]
 
         # Create the native image if it doesn't exist already
         if [[ -v REBUILD_NI || ! -e ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]} ]]; then
-          echo_and_exec ${jdk}/bin/native-image -H:ConfigurationFileDirectories=${OUTPUT}/${benchmark}/conf/${benchmark}-conf -O3 ${ni_opts[$b]} ${jdk_ni_opts[$m]} -o ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]} -cp ${benchmark_jar} org.renaissance.harness.RenaissanceSuite
+          echo_and_exec ${jdk}/bin/native-image -H:ConfigurationFileDirectories=${OUTPUT}/${benchmark}/conf -O3 ${ni_opts[$b]} ${jdk_ni_opts[$m]} -o ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]} -cp ${benchmark_jar} org.renaissance.harness.RenaissanceSuite
         fi
       fi
 
       # And finally run the benchmark as Native Image
-      echo_and_exec ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]} ${JAVA_ARGS} -Dmode=${modes[$m]} ${jdk_runtime_opts[$m]} --json ${OUTPUT}/results/${DATE}/${benchmark}/result-${benchmark}-${modes[$m]}.json -t ${BENCH_TIME} ${benchmark}
+      echo_and_exec ${OUTPUT}/${benchmark}/${benchmark}-${modes[$m]} ${JAVA_ARGS} -Dmode=${modes[$m]} ${jdk_runtime_opts[$m]} ${DOTTY_ARGS} --json ${OUTPUT}/results/${DATE}/${benchmark}/result-${benchmark}-${modes[$m]}.json -t ${BENCH_TIME} ${benchmark}
 
     else # [[ ${modes[$m]} =~ "NI" ]]
 
       # For plain Java modes, just run the benchmarks
-      echo_and_exec ${jdk}/bin/java -jar ${benchmark_jar}  ${JAVA_ARGS} -Dmode=${modes[$m]} ${jdk_runtime_opts[$m]} --json ${OUTPUT}/results/${DATE}/${benchmark}/result-${benchmark}-${modes[$m]}.json -t ${BENCH_TIME} ${benchmark}
+      echo_and_exec ${jdk}/bin/java ${JAVA_ARGS} -Dmode=${modes[$m]} ${jdk_runtime_opts[$m]} -jar ${benchmark_jar} --json ${OUTPUT}/results/${DATE}/${benchmark}/result-${benchmark}-${modes[$m]}.json -t ${BENCH_TIME} ${benchmark}
     fi
   done
 done
